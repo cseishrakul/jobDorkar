6a98c9af056acdd1610d48d2d3363ffea736be52
cseishrakul <ishrakul236@gmail.com>
2025-03-31 02:49:39 +0600

    Ready for deploy

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,8 +3,25 @@ from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 class CustomUserAdmin(UserAdmin):
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        return queryset
+    model = User
+    list_display = ('id', 'username', 'email', 'role', 'is_staff', 'is_active', 'last_login')
+    list_filter = ('role', 'is_staff', 'is_active')
+    fieldsets = (
+        ("User Info", {"fields": ("username", "email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser")}),
+        ("Roles & Resume", {"fields": ("role", "resume")}),
+        ("Important Dates", {"fields": ("last_login", "date_joined")}),
+    )
+
+    add_fieldsets = (
+        ("User Info", {
+            "classes": ("wide",),
+            "fields": ("username", "email", "password1", "password2", "role", "resume", "is_staff", "is_active"),
+        }),
+    )
+
+    search_fields = ("username", "email", "role")
+
+    ordering = ("id",)
 
 admin.site.register(User, CustomUserAdmin)
--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Job,JobApplication
+from .models import Job,JobApplication,JobCategory,Review
 
 # Register your models here.
 admin.site.register(Job)
 admin.site.register(JobApplication)
+admin.site.register(JobCategory)
+admin.site.register(Review)
