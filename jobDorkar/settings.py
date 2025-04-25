3a0a57e244651b7be69866c0deef1e50118a6201
cseishrakul <ishrakul236@gmail.com>
2025-04-25 16:27:01 +0600

    React middleware

--- jobDorkar/settings.py
+++ jobDorkar/settings.py
@@ -43,6 +43,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     "debug_toolbar.middleware.DebugToolbarMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 INTERNAL_IPS = [
