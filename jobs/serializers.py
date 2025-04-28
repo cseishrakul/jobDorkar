9731c60bc0fff9e69d5f3115ea12502eeceecbb6
cseishrakul <ishrakul236@gmail.com>
2025-04-29 00:15:16 +0600

    Profile under work

--- jobs/serializers.py
+++ jobs/serializers.py
@@ -46,7 +46,7 @@ class JobApplicationSerializer(serializers.ModelSerializer):
 class UserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["username", "email","first_name","last_email", "role", "resume"]
+        fields = ["username", "email","first_name","last_name", "role", "resume"]
         
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
